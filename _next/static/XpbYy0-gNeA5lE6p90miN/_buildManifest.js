cbab3a3ef8f4136a1959a146139eb10c3c19d1fb
Areum Jo <areumjo1@gmail.com>
2025-10-17 17:48:54 -0700

    Deploy portfolio to GitHub Pages

---
+++ _next/static/XpbYy0-gNeA5lE6p90miN/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/d1127c1f53f50b20.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/e8895cde7a148993.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/cc3f6a5b645f6872.js",
+  "static/chunks/turbopack-2c2f0d35662bf386.js"
+])
